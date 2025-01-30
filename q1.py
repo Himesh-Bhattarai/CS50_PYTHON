90c89011e10a6cce8f70ea832bf1621c27a08512
Himeshchanchal Bhattarai <144266989+Himesh-Bhattarai@users.noreply.github.com>
2025-01-30 23:03:16 +0545

    just

---
+++ q1.py
@@ -0,0 +1,7 @@
+#write a program to check if a number is even or odd
+user_input = int(input("Write any Number: "))
+
+if user_input % 2 == 0:
+    print("Your Number is Even.")
+else:
+    print("Your Number is Odd.")
