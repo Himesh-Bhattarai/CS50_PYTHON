3e263b0ad316b8cc84a7b7d2a8e109e4507f7b2a
Himeshchanchal Bhattarai <144266989+Himesh-Bhattarai@users.noreply.github.com>
2025-02-05 18:24:31 +0545

    Expension

--- Notes/exp2.py
+++ Notes/exp2.py
@@ -1,11 +1,11 @@
 def main():
-    x = get_int()
+    x = get_int("What's x?")
     print(f' x is {x}')
 
-def get_int():
+def get_int(promt):
     while True:
         try:
-            return int(input("What the value of X? "))
+            return int(input(promt))
         except ValueError:
             pass
         
